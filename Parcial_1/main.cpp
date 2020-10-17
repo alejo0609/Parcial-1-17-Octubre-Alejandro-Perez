80397c91d1bc73540987f6e5e5221e01b74069b3
alejandro.perez9@udea.edu.co <alejandro.perez9@udea.edu.co>
2020-10-17 16:05:57 -0500

    Avance 1

---
+++ Parcial_1/main.cpp
@@ -0,0 +1,59 @@
+/*
+ * Alejandro Pérez Ramírez
+ * C.C 8´029.742
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+#define PI 3.1415926535897932384626433832795
+int main(int argc, char *argv[])
+{
+    double Pos_X_Blanco;
+    double Pos_Y_Blanco;
+    double Velocidad=50;
+    double Pos_X_Canyon;
+    double Pos_Y_Canyon;
+    double Angulo;
+    double Radianes;
+    int intentos;
+    int acierto=0;
+    double DistanciaY;
+    double Gravedad=9.8;
+    float RedondeoDistanciaY;
+    float RedondeoPosY;
+    // Definir variables de inicialización
+    Pos_X_Canyon = 0.0;
+    Pos_Y_Canyon = 0.0;
+    cout<<"\nPosicion X del blanco.....: ";
+    scanf(" %lf",&Pos_X_Blanco);
+    cout<<"Posicion Y del blanco.....: ";
+    scanf(" %lf",&Pos_Y_Blanco);
+    //cout<<"Velocidad Inicial.........: ";
+    //cin>>" %lf",&Velocidad;
+    // Petición de los intentos
+    intentos=0;
+    cout<<"ACIERTO = "<<acierto<<endl;
+    while(intentos<3 && acierto==0)
+    {
+       cout<<"Angulo......: ";
+       cout<<" %lf",&Angulo;
+       Radianes=(Angulo/180.0)*PI;
+      // Calculo de la distancia Y
+      //DistanciaY=(Velocidad*(sin(Radianes))*Tiempo)-((1/2)*Gravedad*(Tiempo*Tiempo));
+      DistanciaY = tan(Radianes)*Pos_X_Blanco-((Gravedad*(Pos_X_Blanco*Pos_X_Blanco))/(2*(Velocidad*Velocidad)*cos(Radianes)*cos(Radianes)));
+       // Si la distancia en Y coincide con la pos Y dada
+       RedondeoDistanciaY=DistanciaY;
+       RedondeoPosY=Pos_Y_Blanco;
+       if ( RedondeoPosY+1>= RedondeoDistanciaY && RedondeoPosY-1<=RedondeoDistanciaY ) {
+          cout<<"Has Acertado\n";
+          acierto=1;
+       }
+       intentos++;
+    }
+  system("No acerto");
+  return 0;
+}
